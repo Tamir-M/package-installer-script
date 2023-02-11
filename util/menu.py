34fa4254180f2d48c8c8bc0de9445f1957c274ca
YotamZiv298 <yotamziv87@gmail.com>
2023-02-11 13:00:40 +0200

    Added doc and updated exit message

--- util/menu.py
+++ util/menu.py
@@ -1,17 +1,29 @@
-import util.download as download
+from util.download import download
 from util.helper import menu_input
 
 
-def menu():
+def menu() -> None:
+    """
+    Package Installer main menu.
+    :return: None
+    """
+
+    open_message: str = "Welcome to Package Installer 📦."
+    options: list[str] = ["Download", "Upload", "Exit"]
+
     while True:
-        user_choice = menu_input('Welcome to Package Installer',
-                                 ['Download', 'Upload', 'Exit'])
+        user_choice: str = menu_input(open_message, options)
 
-        if user_choice is 'Download':
-            download.download()
-
-        if user_choice is 'Upload':
+        if user_choice == "Download":
+            download()
+        elif user_choice == "Upload":
+            # TODO: Add code for uploading functionality
             pass
+        elif user_choice == "Exit":
+            break
 
-        if user_choice is 'Exit':
-            exit(0)
+    print(
+        "Thank you for using Package Installer 📦.\n"
+        "Check out the code repository 👉 https://github.com/Tamir-M/package-installer.\n"
+        "Made by https://github.com/Tamir-M."
+    )
