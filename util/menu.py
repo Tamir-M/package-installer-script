67a8ceb470bcd09fcf4c64e6d21281665fcc37a6
Tamir <tamirmousari@gmail.com>
2023-02-20 17:42:12 +0200

    Fixed strings in menu.py. Organized Imports.

--- util/menu.py
+++ util/menu.py
@@ -1,19 +1,23 @@
-import util.download as download
-from util.helper import menu_input
+from util.download import download
 from util.files import make_input_output_folders
+from util.helper import menu_input
 
 
 def menu():
     make_input_output_folders()
     while True:
-        user_choice = menu_input('Welcome to Package Installer',
-                                 ['Download', 'Upload', 'Exit'])
+        user_choice = menu_input("Welcome to Package Installer",
+                                 ["Download", "Upload", "Exit"])
 
-        if user_choice == 'Download':
-            download.download()
-
-        if user_choice == 'Upload':
+        if user_choice == "Download":
+            download()
+        elif user_choice == "Upload":
             pass
+        elif user_choice == "Exit":
+            break
 
-        if user_choice == 'Exit':
-            exit(0)
+    print(
+        "Thank you for using Package Installer.\n"
+        "Check out the code repository: https://github.com/Tamir-M/package-installer.\n"
+        "Made by: https://github.com/Tamir-M."
+    )
