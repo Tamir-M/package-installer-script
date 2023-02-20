2d48705fe515f32720fa60d4149c37afaba0ab98
Tamir <tamirmousari@gmail.com>
2023-02-20 17:38:28 +0200

    Fixed strings in helper.py

--- util/helper.py
+++ util/helper.py
@@ -2,35 +2,35 @@ import os
 
 
 def is_windows_machine():
-    return os.name == 'nt'
+    return os.name == "nt"
 
 
 def clear_console():
-    os.system('cls' if is_windows_machine() else 'clear')
+    os.system("cls" if is_windows_machine() else "clear")
 
 
 def menu_input(message, options):
     first_time = True
     user_input = ''
-    while not (user_input.isnumeric() and
-               0 < int(user_input) < (len(options)) + 1):
+    while not (user_input.isdigit() and
+               1 <= int(user_input) <= (len(options))):
         clear_console()
         if not first_time:
-            print('incorrect input!')
+            print("incorrect input!")
         else:
             first_time = False
 
         print(message)
 
-        [print(str(index + 1) + '. ' + option)
+        [print(f"{str(index + 1)}. {option}")
          for index, option in enumerate(options)]
 
-        user_input = input("please select one of the above: ")
+        user_input = input("Please select one of the above: ")
 
     clear_console()
     return options[int(user_input) - 1]
 
 
 def confirm(message):
-    user_input = input(message + ' (y): ').lower()
-    return user_input == '' or user_input == 'y' or user_input == 'yes'
+    user_input = input(f"{message} (y): ").lower()
+    return user_input == "" or user_input == "y" or user_input == "yes"
